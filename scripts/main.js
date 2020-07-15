74964da5f6ec29f3e064610cd8b0808bcc477f1b
peggyheiwajima <66979059+peggyheiwajima@users.noreply.github.com>
2020-07-15 14:45:37 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,11 @@
 const myHeading = document.querySelector('h1');
  
 myHeading.textContent = 'Hello world!';
+
+function changeColor(newColor) {
+ 
+  var elem = document.getElementById('myPara');
+ 
+  elem.style.color = newColor;
+ 
+}
